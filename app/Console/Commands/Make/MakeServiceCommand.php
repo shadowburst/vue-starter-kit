e000849b4a6dfcc8fbf829bed40cb3a719db3a43
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-03-02 10:25:01 +0100

    feat(php): better stubs and make:service command

---
+++ app/Console/Commands/Make/MakeServiceCommand.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Console\Commands\Make;
+
+use Illuminate\Console\GeneratorCommand;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputOption;
+
+#[AsCommand(name: 'make:service')]
+class MakeServiceCommand extends GeneratorCommand
+{
+    protected $name = 'make:service';
+
+    protected $description = 'Create a new service class';
+
+    protected $type = 'Service';
+
+    protected function getStub(): string
+    {
+        return $this->resolveStubPath('/stubs/service.stub');
+    }
+
+    protected function resolveStubPath($stub): string
+    {
+        return file_exists($customPath = $this->laravel->basePath(trim($stub, '/')))
+            ? $customPath
+            : __DIR__.'/../..'.$stub;
+    }
+
+    protected function getDefaultNamespace($rootNamespace): string
+    {
+        $namespace = trim($this->option('namespace'), '\\');
+
+        return trim($rootNamespace.'\\'.$namespace, '\\');
+    }
+
+    protected function qualifyClass($name): string
+    {
+        $suffix = trim($this->option('suffix'));
+        if (! empty($suffix) && ! Str::endsWith($name, $suffix)) {
+            $name .= $suffix;
+        }
+
+        return parent::qualifyClass($name);
+    }
+
+    protected function getOptions(): array
+    {
+        return [
+            [
+                'namespace',
+                'N',
+                InputOption::VALUE_REQUIRED,
+                'The namespace (under \App) to place this Service class.',
+                'Services',
+            ],
+            [
+                'suffix',
+                's',
+                InputOption::VALUE_REQUIRED,
+                'Suffix the class with this value.',
+                'Service',
+            ],
+        ];
+    }
+}
