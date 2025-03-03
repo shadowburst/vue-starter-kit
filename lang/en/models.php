5f613824e9028364fcefc5c9d1d7f2507a3cd7d1
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-03-03 20:57:50 +0100

    feat(auth): move to actions

---
+++ lang/en/models.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Models Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain all keys related the the
+    | application's various models.
+    |
+    */
+
+    'user' => [
+        'name' => [
+            'one'  => 'user',
+            'many' => 'users',
+        ],
+        'fields' => [
+            'first_name' => 'first name',
+            'last_name'  => 'last name',
+            'email'      => 'email',
+            'password'   => 'password',
+        ],
+    ],
+
+];
