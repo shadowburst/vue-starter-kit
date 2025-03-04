779bab17805c65513c010aa09a672c4e5d87865c
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-03-04 21:29:27 +0100

    chore(settings): remove base

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,4 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
