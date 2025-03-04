6141196eebdff7f92b83f19d0365ba60acc3af0a
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-03-04 22:47:04 +0100

    feat(dev): add php / typescript type generation

---
+++ resources/js/types/backend.d.ts
@@ -0,0 +1,22 @@
+export type ConfirmPasswordRequest = {
+    password: string;
+};
+export type LoginRequest = {
+    email: string;
+    password: string;
+    remember?: boolean;
+};
+export type NewPasswordRequest = {
+    token: string;
+    email: string;
+    password: string;
+};
+export type PasswordResetRequest = {
+    email: string;
+};
+export type RegisterRequest = {
+    first_name: string;
+    last_name: string;
+    email: string;
+    password: string;
+};
