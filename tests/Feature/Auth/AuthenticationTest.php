7d85468fa886b278a11f8e2a22b046c02511482c
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-03-04 21:29:27 +0100

    chore(settings): remove base

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -19,6 +19,7 @@ class AuthenticationTest extends TestCase
 
     public function test_users_can_authenticate_using_the_login_screen()
     {
+        /** @var User $user */
         $user = User::factory()->create();
 
         $response = $this->post('/login', [
@@ -32,6 +33,7 @@ class AuthenticationTest extends TestCase
 
     public function test_users_can_not_authenticate_with_invalid_password()
     {
+        /** @var User $user */
         $user = User::factory()->create();
 
         $this->post('/login', [
@@ -44,6 +46,7 @@ class AuthenticationTest extends TestCase
 
     public function test_users_can_logout()
     {
+        /** @var User $user */
         $user = User::factory()->create();
 
         $response = $this->actingAs($user)->post('/logout');
