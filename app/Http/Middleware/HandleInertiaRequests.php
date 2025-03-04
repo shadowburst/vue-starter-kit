a22d4ab5a6e4cc769bd1b81dcdbf8773a98fc5b9
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-03-04 21:29:46 +0100

    fix(auth): update tests

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 use Tighten\Ziggy\Ziggy;
@@ -37,13 +36,10 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
-        [$message, $author] = str(Inspiring::quotes()->random())->explode('-');
-
         return [
             ...parent::share($request),
-            'name'  => config('app.name'),
-            'quote' => ['message' => trim($message), 'author' => trim($author)],
-            'auth'  => [
+            'name' => config('app.name'),
+            'auth' => [
                 'user' => $request->user(),
             ],
             'ziggy' => [
