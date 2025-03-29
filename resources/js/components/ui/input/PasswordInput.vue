d0a7babde2488d81cb2b9b3b7a2bedf6fe571968
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2025-03-29 20:25:20 +0100

    fix(component): inherit attributes correctly for password input

--- resources/js/components/ui/input/PasswordInput.vue
+++ resources/js/components/ui/input/PasswordInput.vue
@@ -6,6 +6,10 @@ import { computed, ref } from 'vue';
 import BaseInput from './BaseInput.vue';
 import { InputEmits, InputProps } from './interface';
 
+defineOptions({
+    inheritAttrs: false,
+});
+
 const props = defineProps<InputProps>();
 
 const emits = defineEmits<InputEmits>();
@@ -21,7 +25,7 @@ function toggle() {
 
 <template>
     <div class="relative w-full">
-        <BaseInput class="pr-10" v-bind="forwarded" :type />
+        <BaseInput class="pr-10" v-bind="{ ...forwarded, ...$attrs }" :type />
         <Button class="absolute inset-y-0 end-0" variant="ghost" size="icon" @click="toggle()">
             <component class="size-6" :is="hidden ? EyeOffIcon : EyeIcon" />
         </Button>
