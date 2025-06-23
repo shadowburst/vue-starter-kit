24a9dd3c769b4d2d3c69d8849f7f67ecc65f98c0
pbaudry <p.baudry@lynx-business.com>
2025-06-23 10:15:22 +0200

    fix(ui): hide indicator for single combobox input

--- resources/js/components/ui/combobox/ComboboxItemIndicator.vue
+++ resources/js/components/ui/combobox/ComboboxItemIndicator.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
 import { Checkbox } from '@/components/ui/checkbox';
-import { ComboboxItemIndicatorProps, injectComboboxItemContext, useForwardProps } from 'reka-ui';
+import {
+    ComboboxItemIndicatorProps,
+    injectComboboxItemContext,
+    injectComboboxRootContext,
+    useForwardProps,
+} from 'reka-ui';
 
 const props = defineProps<ComboboxItemIndicatorProps>();
 const forwarded = useForwardProps(props);
 
+const { multiple, modelValue } = injectComboboxRootContext();
 const { isSelected } = injectComboboxItemContext();
 </script>
 
 <template>
-    <Checkbox v-bind="forwarded" :model-value="isSelected" />
+    <Checkbox
+        v-if="multiple || modelValue"
+        v-bind="forwarded"
+        :model-value="isSelected"
+        :class="{ invisible: !multiple && !isSelected }"
+    />
 </template>
