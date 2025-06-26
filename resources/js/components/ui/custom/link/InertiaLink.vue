c799dd9f8985b09dfcf014d33e987127876db1c6
pbaudry <p.baudry@lynx-business.com>
2025-06-26 11:46:09 +0200

    fix(ui): don't prefetch non-get links

--- resources/js/components/ui/custom/link/InertiaLink.vue
+++ resources/js/components/ui/custom/link/InertiaLink.vue
@@ -21,10 +21,11 @@ const forwarded = useForwardProps(delegatedProps);
 const disabled = computed((): LinkProps['disabled'] => props.disabled || undefined);
 const as = computed((): LinkProps['as'] => (props.disabled ? 'button' : props.as));
 const href = computed((): LinkProps['href'] => (props.disabled || !props.href ? '#' : props.href));
+const prefetch = computed((): LinkProps['prefetch'] => props.prefetch && (!props.method || props.method === 'get'));
 </script>
 
 <template>
-    <Link v-bind="forwarded" :disabled :as :href>
+    <Link v-bind="forwarded" :disabled :as :href :prefetch>
         <slot />
     </Link>
 </template>
