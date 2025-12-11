3d45816eb2d2ab443c2c3c8cba89b956e89d1b2e
Julian Schramm <hi@julian.center>
2025-12-11 09:51:05 +0100

    fix svg attributes in Welcome page (#227)

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -86,8 +86,8 @@ withDefaults(
                                 >
                                     <span>Documentation</span>
                                     <svg
-                                        width="{10}"
-                                        height="{11}"
+                                        width="10"
+                                        height="11"
                                         viewBox="0 0 10 11"
                                         fill="none"
                                         xmlns="http://www.w3.org/2000/svg"
@@ -125,8 +125,8 @@ withDefaults(
                                 >
                                     <span>Laracasts</span>
                                     <svg
-                                        width="{10}"
-                                        height="{11}"
+                                        width="10"
+                                        height="11"
                                         viewBox="0 0 10 11"
                                         fill="none"
                                         xmlns="http://www.w3.org/2000/svg"
